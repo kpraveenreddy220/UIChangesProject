bc8ab261024bcd44161f6efcf45303aa4e7c6b12
pkarnati <pkarnati@PS-PKARNATI.nbspaymentsolutions.com>
2018-02-28 20:15:48 -0500

    new login form

--- src/web/javascript/app.js
+++ src/web/javascript/app.js
@@ -1,38 +1,20 @@
+//document.addEventListener('DOMContentLoaded', fn, false);
 $(document).ready(function(){
-	var result = function(param1, param2, param3){
-		return "Hello, This is " +  param1 + " and your pwd is " + param2 + " and aged " + param3;
+	var result = function(param1, param2){
+		return "Hello, This is " +  param1 + " and your pwd is " + param2;
 	};
-	var database = [{name: "praveen", pwd: "test", age: 28}, 
-		{name: "pradeep", pwd: "hello", age: 25},
-		{name: "test", pwd: "testing", age: 29}];
+	var database = [{name: "praveen", pwd: "test"}, 
+		{name: "pradeep", pwd: "hello"},
+		{name: "test", pwd: "testing"}];
 	
-	var username = prompt("enter your username");
-	var response;
+	document.getElementById("submit").addEventListener("click", myFunction);
+	function myFunction(){
+		var username = document.getElementById("username").value;
+		var pwd = document.getElementById("pwd").value;
+		document.getElementById("demo").innerHTML = result(username, pwd);
+	}
+	/*var response;
 	for(var i=0; i< database.length; i++){ // i =0, database.length = 3
-		
-		/*for(key in database[i]){  // database[i=0] = {name: "praveen", pwd: "test", age: 28}
-			if(database[i][key] === username){  //key = praveen 
-				var pwd = prompt("enter your pwd");
-				if(pwd === database[i].pwd){
-					var age = prompt("enter your age");
-					if(age === database[i].age){
-						response = result();
-						break;
-					} else {
-						response = "wrong age";
-						break;
-					}
-				} else {
-					response = "wrong pwd";
-					break;
-				}
-			} else {
-				response = "wrong username";
-				break;
-			}
-			break;
-		}
-		*/
 		var obj = database[i];
 		if(obj.name === username){
 			var pwd = prompt("enter your pwd");
@@ -40,7 +22,7 @@ $(document).ready(function(){
 				var age = prompt("enter your age");
 				if(age === String(database[i].age)){
 					//response = result();
-					response = result(username, pwd, age);
+					response = result(username +"123", pwd, age);
 					break;
 				} else {
 					response = "wrong age";
@@ -52,7 +34,7 @@ $(document).ready(function(){
 		} else {
 			response = "wrong credentials";
 		}
-		
-	}
-	document.getElementById("demo").innerHTML = response;
+	}*/
+	/*document.getElementById("demo").innerHTML = response;
+	document.getElementById("demo1").innerHTML = "ksdjflksjdflksdjfsad lksdjfklasdjflsdakj";*/
 });
